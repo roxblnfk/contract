f5174298101147687663ab66865ddec9a642004a
roxblnfk <roxblnfk@ya.ru>
2021-04-23 12:50:06 +0300

    PipelineResolverInterface::resolvePipeline has now variadic param

--- src/Pipeline/PipelineResolverInterface.php
+++ src/Pipeline/PipelineResolverInterface.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace roxblnfk\Contract\Pipeline;
 
-use roxblnfk\Contract\Pipeline\PipelineInterface;
-
 interface PipelineResolverInterface
 {
-    public function resolvePipeline(PipelineInterface $pipeline): callable;
+    public function resolvePipeline(PipelineInterface ...$pipeline): callable;
 }
