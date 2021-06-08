9c29a77c96a92566425e9dc48a8c3a4b6934bb5e
roxblnfk <roxblnfk@ya.ru>
2021-06-08 19:09:03 +0300

    GlueRequestHandler now is anonymous

--- implementation/Pipeline/MiddlewaresPipelineResolver.php
+++ implementation/Pipeline/MiddlewaresPipelineResolver.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace roxblnfk\Contract\Implementation\Pipeline;
 
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use roxblnfk\Contract\Implementation\Pipeline\Exception\DefaultRequestHandlerIsNotConfiguredException;
@@ -34,7 +35,58 @@ final class MiddlewaresPipelineResolver implements PipelineResolverInterface
     public function resolvePipeline(PipelineInterface|iterable ...$pipelines): callable
     {
         $iterator = $this->iteratePipelines($pipelines);
-        return new GlueRequestHandler($iterator, $this->injector);
+        return $this->createHandler($iterator);
+    }
+
+    private function createHandler(\Generator $iterator): callable
+    {
+        return new class ($iterator, $this->injector) implements RequestHandlerInterface
+        {
+            private Injector $injector;
+            private \Generator $iterator;
+
+            public function __construct(\Generator $iterator, Injector $injector)
+            {
+                $this->iterator = $iterator;
+                $this->injector = $injector;
+            }
+
+            final public function __invoke(ServerRequestInterface $request): ResponseInterface
+            {
+                return $this->handle($request);
+            }
+
+            final public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                if (!$this->iterator->valid()) {
+                    /** @var RequestHandlerInterface $handler */
+                    $handler = $this->iterator->getReturn();
+                    return $handler->handle($request);
+                }
+                $middleware = $this->iterator->current();
+                $this->iterator->next();
+                $nextHandler = new self($this->iterator, $this->injector);
+
+                if ($middleware instanceof MiddlewareInterface) {
+                    return $middleware->process($request, $nextHandler);
+                }
+                if (is_callable($middleware)) {
+                    $result = $this->injector->invoke($middleware, [$request, $nextHandler]);
+                    // Kostyl' for yiisoft/router
+                    if ($result instanceof MiddlewareInterface) {
+                        return $result->process($request, $nextHandler);
+                    }
+                } else {
+                    throw new InvalidMiddlewareDefinitionException();
+                }
+                if (!$result instanceof ResponseInterface) {
+                    throw new InvalidMiddlewareDefinitionException(
+                        sprintf('Middleware MUST return instance of %s.', ResponseInterface::class)
+                    );
+                }
+                return $result;
+            }
+        };
     }
 
     /**
