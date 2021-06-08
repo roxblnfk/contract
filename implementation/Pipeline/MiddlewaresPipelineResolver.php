7ef4403ed284ecedec907fa1a1a1b546db86ec23
roxblnfk <roxblnfk@ya.ru>
2021-06-08 19:22:09 +0300

    Middlewares caching in the middlewares dispatcher

--- implementation/Pipeline/MiddlewaresPipelineResolver.php
+++ implementation/Pipeline/MiddlewaresPipelineResolver.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace roxblnfk\Contract\Implementation\Pipeline;
 
+use Generator;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -38,14 +39,16 @@ final class MiddlewaresPipelineResolver implements PipelineResolverInterface
         return $this->createHandler($iterator);
     }
 
-    private function createHandler(\Generator $iterator): callable
+    private function createHandler(Generator $iterator): callable
     {
         return new class ($iterator, $this->injector) implements RequestHandlerInterface
         {
             private Injector $injector;
-            private \Generator $iterator;
+            private ?Generator $iterator;
+            private ?MiddlewareInterface $middleware = null;
+            private ?RequestHandlerInterface $nextHandler = null;
 
-            public function __construct(\Generator $iterator, Injector $injector)
+            public function __construct(Generator $iterator, Injector $injector)
             {
                 $this->iterator = $iterator;
                 $this->injector = $injector;
@@ -58,23 +61,31 @@ final class MiddlewaresPipelineResolver implements PipelineResolverInterface
 
             final public function handle(ServerRequestInterface $request): ResponseInterface
             {
-                if (!$this->iterator->valid()) {
-                    /** @var RequestHandlerInterface $handler */
-                    $handler = $this->iterator->getReturn();
-                    return $handler->handle($request);
+                if ($this->middleware === null) {
+                    if ($this->iterator === null) {
+                        return $this->nextHandler->handle($request);
+                    }
+                    if (!$this->iterator->valid()) {
+                        /** @var RequestHandlerInterface $handler */
+                        $this->nextHandler = $this->iterator->getReturn();
+                        $this->iterator = null;
+                        return $this->nextHandler->handle($request);
+                    }
+                    $this->middleware = $this->iterator->current();
+                    $this->nextHandler = new self($this->iterator, $this->injector);
+                    $this->iterator->next();
+                    $this->iterator = null;
                 }
-                $middleware = $this->iterator->current();
-                $this->iterator->next();
-                $nextHandler = new self($this->iterator, $this->injector);
 
-                if ($middleware instanceof MiddlewareInterface) {
-                    return $middleware->process($request, $nextHandler);
+                if ($this->middleware instanceof MiddlewareInterface) {
+                    return $this->middleware->process($request, $this->nextHandler);
                 }
-                if (is_callable($middleware)) {
-                    $result = $this->injector->invoke($middleware, [$request, $nextHandler]);
+                if (is_callable($this->middleware)) {
+                    $result = $this->injector->invoke($this->middleware, [$request, $this->nextHandler]);
                     // Kostyl' for yiisoft/router
                     if ($result instanceof MiddlewareInterface) {
-                        return $result->process($request, $nextHandler);
+                        $this->middleware = $result;
+                        return $result->process($request, $this->nextHandler);
                     }
                 } else {
                     throw new InvalidMiddlewareDefinitionException();
@@ -94,7 +105,7 @@ final class MiddlewaresPipelineResolver implements PipelineResolverInterface
      *
      * @throws InvalidMiddlewareDefinitionException
      */
-    private function iteratePipelines(array $pipelines): \Generator
+    private function iteratePipelines(array $pipelines): Generator
     {
         foreach ($pipelines as $pipeLine) {
             $definitions = $pipeLine instanceof PipelineInterface ? $pipeLine->getPipes() : $pipeLine;
